b0bfbc95b69bc1d58d23ec4259059445f2b530f6
Jeffrey Martinez <martinezjf2@gmail.com>
2019-12-18 02:47:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@
 def prime?(contender)
   
   return false if contender < 2
-  array = (2..value).to_a - [contender]
+  array = (2..contender).to_a - [contender]
 
   array.each do |num|
     return false if contender % num == 0
